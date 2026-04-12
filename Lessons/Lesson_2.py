ff4eb3899e5ad1678003b384d3ef7a4e2448eef0
marlenlab <marlenmarlisovich@gmail.com>
2026-04-12 19:00:14 +0600

    Added hw 4,5

--- Lessons/Lesson_2.py
+++ Lessons/Lesson_2.py
@@ -1,6 +1,5 @@
 # Наследование
 
-
 # Родительский| Супер класс
 class Hero:
 
@@ -13,8 +12,60 @@ class Hero:
         return f"{self.name} base action"
 
 
-kirito= Hero("Kirito", 1, 100)
+# Kirito= Hero("Kirito", 1, 100)
 
 
+class MageHero(Hero):
+    def __init__(self, name, level, health, mp):
+        super().__init__(name, level, health,)
+        self.mp = mp
+
+    def action(self):
+        print(f" Hi i'am {self.name} base action")
+
+kirito = Hero("Kirito", 100, 1000)
+asuna = MageHero("Asuno", 100, 1000, 100)
+
+# print(kirito.action())
+# print(asuna.action())
+
+class Fly:
+    @staticmethod
+    def action():
+        return f" Fly "
+
+class Swim:
+    @staticmethod
+    def action ():
+        return f"Swim "
+
+class Animals(Fly,Swim):
+    pass
+
+# duck = Animals()
+# print(duck.action())
 
 
+class A:
+    def action(self):
+        print("A")
+
+class B(A):
+    def action(self):
+        super().action()
+        print("B")
+
+class C(A):
+    def action(self):
+        super().action()
+        print("C")
+
+class D(B,C):
+    def action(self):
+        super().action()
+        print("D")
+
+TestObj = D()
+TestObj.action()
+
+print(D.__mro__)
